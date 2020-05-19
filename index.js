ecdcc651a0aa27da542324135a7effbb51299ef2
spacecityjams <62345526+spacecityjams@users.noreply.github.com>
2020-05-19 12:44:18 -0500

    changed to read input from file

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var input = "James";
+var fs = require("fs");
 
-    for (var i = 0; i < input.length; i++) {
-        console.log(input.charAt(i) + ',');
+fs.readFile('input.txt', function (err, data) {
+    if (err) {
+       return console.error(err);
+    }
+    console.log("Asynchronous read: " + data.toString());
+    for (var i = 0; i < data.toString().length; i++) {
+        console.log(data.toString().charAt(i) + ',');
       }
+ });
 
