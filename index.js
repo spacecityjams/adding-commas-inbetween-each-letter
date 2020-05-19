5dd538a6cbeb5ee3b03f8b2d99571dc63921f531
spacecityjams <62345526+spacecityjams@users.noreply.github.com>
2020-05-19 14:57:54 -0500

    outputs file as designed

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ fs.readFile('input.txt', function (err, data) {
     if (err) {
        return console.error(err);
     }
-    console.log("Asynchronous read: " + data.toString());
-    var newArray = data.toString().split("")
-    console.log(newArray)
+//    console.log("Asynchronous read: " + data.toString());
+    var inputArray = data.toString().split("");
+//    console.log(inputArray);
 
-/*
+    var outputArray = []
+    for (i = 0; i < inputArray.length; i++){
+        outputArray.push(inputArray[i]);
+        outputArray.push(",");
+    }
 
-    for (var i = 0; i < newArray.length; i++) {
-        console.log(data.toString().charAt(i) + ',');
-        fs.writefile
-        
-      }
+    outputArray.pop();
+//    console.log(outputArray)
+
+    for (j = 0; j < outputArray.length; j++){
+        fs.writeFileSync('output.txt', outputArray.join(""))
+    }   
  });
-*/
-});
